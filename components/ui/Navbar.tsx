137feba1cb48ad7c606e517e35c8def0013d8c3c
TheBigNerd <ethanpotter0123@gmail.com>
2025-05-14 22:12:34 +0100

    Enhance Navbar styling

--- components/ui/Navbar.tsx
+++ components/ui/Navbar.tsx
@@ -3,7 +3,7 @@ import React from 'react';
 
 const Navbar = () => {
     return(
-        <nav className='z-15 top-15 left-1/2 -translate-x-1/2 fixed flex items-center gap-5 py-2 px-4 rounded-full bg-black/60 backdrop-blur-md border text-white border-zinc-800'>
+        <nav className='z-15 top-15 left-1/2 -translate-x-1/2 fixed flex  justify-center items-center gap-5 py-2 px-4 md:px-6 rounded-full bg-black/60 backdrop-blur-md border text-white border-zinc-800 w-auto max-w-[95%]'>
             <Link href="/" className='hover:text-gray-300 transition-colors'>
                 <span>
                     Something
@@ -14,8 +14,8 @@ const Navbar = () => {
                     Something
                 </span>
             </Link>
-            <a href="">
-                <h1 className='text-6xl font-bold text-white'>Nerd Dev</h1>
+            <a href="" className="flex justify-center w-full md:w-auto">
+                <h1 className='text-4xl md:text-6xl font-bold text-white text-center'>Nerd Dev</h1>
             </a>
             <Link href="/" className='hover:text-gray-300 transition-colors'>
                 <span>
