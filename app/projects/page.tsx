631c70bc376bbc4e3242e177e15649b8cd250311
TheBigNerd <ethanpotter0123@gmail.com>
2025-05-18 19:36:07 +0100

    Add LeetCode and Projects pages; update Navbar links and dependencies adding Framer Motion for future use on Navbar

---
+++ app/projects/page.tsx
@@ -0,0 +1,5 @@
+export default function projects() {
+  return (
+    <div className="min-h-screen w-full flex items-center justify-center bg-black"></div>
+  )
+}
--- components/ui/Navbar.tsx
+++ components/ui/Navbar.tsx
@@ -4,15 +4,15 @@ import React from 'react';
 const Navbar = () => {
     return(
         <nav className='z-15 top-15 left-1/2 -translate-x-1/2 fixed flex  justify-center items-center gap-5 py-2 px-4 md:px-6 rounded-full bg-black/60 backdrop-blur-md border text-white border-zinc-800 w-auto max-w-[95%]'>
-            <Link href="/" className='hover:text-gray-300 transition-colors'>
+            <Link href="/projects" className='hover:text-gray-300 transition-colors'>
                 <span>
                     Projects
                 </span>
             </Link>
-            <a href="" className="flex justify-center w-full md:w-auto">
+            <Link href="/" className="flex justify-center w-full md:w-auto">
                 <img src="/logo.png" alt="Logo" className='w-30 h-30 rounded-full' />
-            </a>
-            <Link href="/" className='hover:text-gray-300 transition-colors'>
+            </Link>
+            <Link href="/leetcode" className='hover:text-gray-300 transition-colors'>
                 <span>
                     Leet Code
                 </span>
