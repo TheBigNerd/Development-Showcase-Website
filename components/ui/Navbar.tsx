3284adc17a7eba2d21b887dc1eebc61ddb1e2436
TheBigNerd <ethanpotter0123@gmail.com>
2025-05-18 19:19:07 +0100

    Added Logo to NavBar, named two page sections of Projects and Leet Code.

--- components/ui/Navbar.tsx
+++ components/ui/Navbar.tsx
@@ -6,25 +6,15 @@ const Navbar = () => {
         <nav className='z-15 top-15 left-1/2 -translate-x-1/2 fixed flex  justify-center items-center gap-5 py-2 px-4 md:px-6 rounded-full bg-black/60 backdrop-blur-md border text-white border-zinc-800 w-auto max-w-[95%]'>
             <Link href="/" className='hover:text-gray-300 transition-colors'>
                 <span>
-                    Something
-                </span>
-            </Link>
-            <Link href="/" className='hover:text-gray-300 transition-colors'>
-                <span>
-                    Something
+                    Projects
                 </span>
             </Link>
             <a href="" className="flex justify-center w-full md:w-auto">
-                <h1 className='text-4xl md:text-6xl font-bold text-white text-center'>Nerd Dev</h1>
+                <img src="/logo.png" alt="Logo" className='w-30 h-30 rounded-full' />
             </a>
             <Link href="/" className='hover:text-gray-300 transition-colors'>
                 <span>
-                    Something
-                </span>
-            </Link>
-            <Link href="/" className='hover:text-gray-300 transition-colors'>
-                <span>
-                    Something
+                    Leet Code
                 </span>
             </Link>
         </nav>
