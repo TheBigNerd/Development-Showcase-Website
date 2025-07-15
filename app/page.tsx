9963d653d43d126ec6a022f15e6b6c408c78e97f
TheBigNerd <ethanpotter0123@gmail.com>
2025-07-15 13:56:51 +0100

    Remove unused imports

--- app/page.tsx
+++ app/page.tsx
@@ -1,8 +1,3 @@
-
-import Link from 'next/link';
-import { Card, CardContent, CardHeader, CardTitle, CardDescription } from '@/components/ui/card';
-import Image from 'next/image';
-
 export default function Home() {
   return (
     <div className="min-h-screen w-full flex items-center justify-center bg-black p-8 select-text">
